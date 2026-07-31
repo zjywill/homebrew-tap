6101022449a69b11dc084a6f9e47ed01b30c0ed0
zjywill <zjywill@hotmail.com>
2026-07-31 16:27:58 +0800

    pickroom 0.1.0

---
+++ Formula/pickroom.rb
@@ -0,0 +1,52 @@
+class Pickroom < Formula
+  desc "Keyboard-first RAW culling workspace for macOS"
+  homepage "https://github.com/zjywill/Pickroom"
+  url "git@github.com:zjywill/Pickroom.git",
+      tag:      "v0.1.0",
+      revision: "9a2877c515b4dde3f579485b9ec7e3036ec844f4"
+  version "0.1.0"
+
+  livecheck do
+    url :stable
+    regex(/^v?(\d+(?:\.\d+)+)$/i)
+    strategy :git
+  end
+
+  depends_on xcode: ["16.0", :build]
+  depends_on macos: :sonoma
+
+  def install
+    ENV["UNIVERSAL"] = "0"
+    ENV["DMG"] = "0"
+    ENV["DEST"] = buildpath/"dist"
+    ENV["VERSION"] = version.to_s
+    system "./scripts/bundle.sh"
+
+    prefix.install "dist/Pickroom.app"
+    (bin/"pickroom").write <<~SH
+      #!/bin/bash
+      exec "#{opt_prefix}/Pickroom.app/Contents/MacOS/Pickroom" "$@"
+    SH
+  end
+
+  def caveats
+    <<~EOS
+      Pickroom.app was installed to:
+        #{opt_prefix}/Pickroom.app
+
+      Put a real copy in Applications so Finder, Launchpad, and Spotlight see it:
+        rm -rf /Applications/Pickroom.app
+        cp -R #{opt_prefix}/Pickroom.app /Applications/Pickroom.app
+
+      Repeat the copy after each upgrade, with Pickroom quit first.
+    EOS
+  end
+
+  test do
+    app = prefix/"Pickroom.app"
+    assert_path_exists app/"Contents/MacOS/Pickroom"
+    system "/usr/bin/codesign", "--verify", "--deep", "--strict", app
+    assert_match "com.junyizhang.Pickroom",
+                 shell_output("/usr/bin/defaults read #{app}/Contents/Info CFBundleIdentifier")
+  end
+end

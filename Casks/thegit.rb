9da395a73306f1ea4dceeee03a22f61581676986
zjywill <zjywill@hotmail.com>
2026-08-19 17:25:24 +0800

    thegit 0.10.9: notarised DMG

--- Casks/thegit.rb
+++ Casks/thegit.rb
@@ -1,6 +1,6 @@
 cask "thegit" do
   version "0.10.9"
-  sha256 "27df25eaf0b7fcbbfe7542529148bc0d95af57912dec65a1092adeefefba90ba"
+  sha256 "460a34ea1743a241016082f2d9dece616a54976f2aa656ce13585a0655eaaaab"
 
   url "https://github.com/zjywill/TheGit/releases/download/v#{version}/TheGit-#{version}.dmg"
   name "TheGit"
